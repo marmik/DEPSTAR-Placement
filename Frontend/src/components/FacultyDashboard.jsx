408863fa99a50064a498ef1d0b4fcc96cc822502
princebavishi <princekbavishi@gmail.com>
2024-05-18 10:18:50 +0530

    add FD Routes ...

--- Frontend/src/components/FacultyDashboard.jsx
+++ Frontend/src/components/FacultyDashboard.jsx
@@ -28,7 +28,15 @@ function FacultyDashboard() {
           <div className='bg-slate-200  flex-wrap gap-10 flex justify-between items-center sticky top-0 w-full p-7'>
             <div className='flex justify-center  items-center gap-10'>
               <GiHamburgerMenu className='text-2xl cursor-pointer ' onClick={toggleSidebar} />
-              <h2 className='font-bold text-2xl'>Dashboard</h2>
+              <h2 className='font-bold text-2xl'>
+              <Routes>
+                  <Route path='/' element={'Faculty Dashboard'}></Route>
+                  <Route path='/add-exam' element={'Add Exam'}></Route>
+                  <Route path='/manage-exams' element={'Manage Exams'}></Route>
+                  <Route path='/manage-feedbacks' element={'Manage Feedbacks'}></Route>
+                  <Route path='/system-feedbacks' element={'System Feedbacks'}></Route>
+                </Routes>
+              </h2>
             </div>
             <div className='flex justify-center items-center gap-4'>
               <p className='font-medium text-xl'>Raj Markana</p>
